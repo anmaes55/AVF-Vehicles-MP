404af62b2d27c488503f31356fb7394f936b9aa5
Juan Ruiz <juanruiz.lo@gmail.com>
2026-05-10 22:53:48 +0200

    target tracks moving_range_target actualizado a la nueva version de Teardown MP

--- target_tracks/moving_range_target.lua
+++ target_tracks/moving_range_target.lua
@@ -3,7 +3,7 @@
 
 track_distance = 2
 
-function init() 
+local function initTrack()
 	track_driver = FindJoint("track_driver")
 	turn_dial = FindJoint("turn_dial")
 
@@ -20,27 +20,42 @@ function init()
 	shift_time = 1
 end
 
-
-function tick()
+local function tickTrack()
 	-- DebugWatch("track movement",GetJointMovement(track_driver))
 	-- DebugWatch("track dist ",track_distance)
 	-- DebugWatch("side  ",side)
-	if(GetJointMovement(track_driver)<(track_driver_min+track_driver_max*(1-change_dist)) and side =="max") then 
+	if(GetJointMovement(track_driver)<(track_driver_min+track_driver_max*(1-change_dist)) and side =="max") then
 		SetJointMotorTarget(turn_dial,turn_dial_min+turn_dial_max*(1-change_dist),track_speed)
 		SetValue("track_distance",2,"linear",shift_time)
 		side = "min"
-	elseif(GetJointMovement(track_driver)>(track_driver_max*change_dist) and side =="min") then 
+	elseif(GetJointMovement(track_driver)>(track_driver_max*change_dist) and side =="min") then
 		SetJointMotorTarget(turn_dial,turn_dial_max*change_dist,track_speed)
 		SetValue("track_distance",0,"linear",shift_time)
 		side = "max"
 	end
 
-	if(track_distance == 0) then 
+	if(track_distance == 0) then
 		SetJointMotorTarget(track_driver,track_driver_min+track_driver_max*(1-max_dist),track_speed)
 		track_distance=1
-	elseif(track_distance ==2) then 
+	elseif(track_distance ==2) then
 
 		SetJointMotorTarget(track_driver,track_driver_max*max_dist,track_speed)
 		track_distance=1
 	end
+end
+
+function init()
+	initTrack()
+end
+
+function tick()
+	tickTrack()
+end
+
+function server.init()
+	initTrack()
+end
+
+function server.tick()
+	tickTrack()
 end
